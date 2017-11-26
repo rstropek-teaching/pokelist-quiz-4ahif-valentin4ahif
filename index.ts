52286e7db41763f80b2d36605e61024f16809164
valentin4ahif <32124205+valentin4ahif@users.noreply.github.com>
2017-11-26 14:58:45 +0100

    improved and formatted code

--- index.ts
+++ index.ts
@@ -1,84 +1,75 @@
-let offset=0;
+let offset = 0;
 let pokelist;
 listPokemons();
-
-//Class representing a Pokemon
-class Pokemon{
-    name : String;
-    image : String;
-    weight : number;
-    abilities : Array<String>;
-
-    constructor(name : String, image : String, weight : number, abilities : Array<String>){
-        this.name=name;
-        this.image=image;
-        this.weight=weight;
-        this.abilities=abilities;
-    }
-}
 
 //loads the list of pokemon and generates the buttons
 async function listPokemons() {
     $("#btnBack").hide();
+    $("#singleHeading").hide();
     pokelist = await $.get(`https://pokeapi.co/api/v2/pokemon/?limit=20&offset=${offset}`);
 
     let html = '';
-    for(const pokemon of pokelist.results) {
+    for (const pokemon of pokelist.results) {
         html += `<tr><td>${pokemon.name}</td><td><button type="button" onclick="showPokeDetails('${pokemon.url}')">Details</button></td></tr>`
     }
-    
+
     $('#pokemons')[0].innerHTML = html;
 };
 
 //loads and shows the details of a specific pokemon
-function showPokeDetails(pokeURL){
-    let html= '';
+function showPokeDetails(pokeURL) {
+    let html = '';
     let pokeDetails;
 
-    (async function (){
+    (async function () {
         pokeDetails = await $.get(pokeURL);
-        let pokemon=new Pokemon(pokeDetails.name, pokeDetails.sprites.front_default, pokeDetails.weight, pokeDetails.abilities);
 
-        html=`<tr><td>Name: ${pokemon.name}</td></tr><tr><td>Foto: <br><img src="${pokemon.image}" alt="Picture of Pokemon"></td></tr>`+
-        `<tr><td>Weight: ${pokemon.weight}</td></tr><tr><td>Abilities:</td></tr><td><ul>`
+        html = `<tr><td>Name: ${pokeDetails.name}</td></tr><tr><td>Photo: <br><img src="${pokeDetails.sprites.front_default}"` +
+            `alt="Picture of Pokemon"></td></tr>` +
+            `<tr><td>Weight: ${pokeDetails.weight}</td></tr><tr><td>Abilities:</td></tr><td><ul>`
 
-        for(let ability of pokeDetails.abilities){
-            html+=`<li>${ability.ability.name}</li>`
+        for (let ability of pokeDetails.abilities) {
+            html += `<li>${ability.ability.name}</li>`
         }
-        html+=`</ul></td>`
+        html += `</ul></td>`
 
         $("#pokemons").hide();
         $("#btnPrev").hide();
         $("#btnNext").hide();
+        $("#listHeading").hide();
+
+        $("#singleHeading").show();
         $("#btnBack").show();
         $("#pokemonDetail").show();
-        
+
         $('#pokemonDetail')[0].innerHTML = html;
     })();
 }
 
 //substrakts the offset -20 to get the previous page
-function btnPrevClick(){
-    if(offset>0){
-        offset-=20;
+function btnPrevClick() {
+    if (offset > 0) {
+        offset -= 20;
         listPokemons();
     }
-    else{
+    else {
         window.alert("no previous page left");
     }
 }
 
 //adds the offset +20 to get the next page
-function btnNextClick(){
-    offset+=20;
+function btnNextClick() {
+    offset += 20;
     listPokemons();
 }
 
 //hides all information that is not used and shows the information that is used
-function btnBackClick(){
+function btnBackClick() {
     $("#pokemonDetail").hide();
     $("#btnBack").hide();
+    $("#singleHeading").hide();
 
+    $("#listHeading").show();
     $("#pokemons").show();
     $("#btnPrev").show();
     $("#btnNext").show();
